528fdec60cc173ee6ed35198466b80bf8984754a
Riccardo Germenia <rgermenia@fbk.eu>
2025-02-04 22:30:36 +0100

    improve visitor and grammar to handle branches

--- visitor.py
+++ visitor.py
@@ -1,5 +1,5 @@
 from copy import deepcopy
-from lark import Visitor
+from lark import Visitor, Token, Tree
 from utils import children_to_string
 
 
@@ -11,32 +11,65 @@ class MessagesFinder(Visitor):
     _current_process = None
     _actual_statement = None
     _current_channel = None
+    _current_depth = 0
+    _parenthesis_counter = 0
+    _single_line_in_branch = False
     _func_content_counter = 0
+    _parenthesis_list = []
+    _channels = set()
 
     def assignment(self, tree):
         if self._current_process is None:
             self._current_process = tree.children[0].children[0].value
             self._processes[self._current_process] = []
 
+    def channel_declaration(self, tree):
+        channels = children_to_string(tree)
+        if tree.children[-1].value == "private":
+            channels = channels.removesuffix("private")
+        channels = [c.strip() for c in channels.split(",")]
+        self._channels.update(channels)
+
     def statement(self, tree):
-        if tree.children[0].data != "func_content":
+        parenthesis_found = False
+        if isinstance(tree.children[0], Token):
+            if tree.children[0].value == "(" and self._actual_statement in ["then",
+                                                                            "else_statement"]:
+                self._parenthesis_list.append(self._parenthesis_counter)
+                self._parenthesis_counter += 1
+                parenthesis_found = True
+            elif tree.children[0].value == ")":
+                self._parenthesis_counter -= 1
+                if self._parenthesis_counter in self._parenthesis_list:
+                    self._parenthesis_list.remove(self._parenthesis_counter)
+                    self._current_depth -= 1
+        if not parenthesis_found and self._actual_statement in ["then", "else_statement"] and \
+            tree.children[0].data != self._actual_statement:
+            self._single_line_in_branch = True
+        elif parenthesis_found:
+            self._actual_statement = "LPAR"
+        if isinstance(tree.children[0], Tree) and tree.children[0].data != "func_content":
             self._actual_statement = tree.children[0].data
 
     def func_content(self, tree):
         if self._actual_statement in ["in", "out"]:
             if self._current_channel is None and len(tree.children) == 1:
-                # todo: check if the channel is valid
                 self._current_channel = tree.children[0].value
-                message_dict = {
-                    "statement": self._actual_statement,
-                    "channel": self._current_channel,
-                    "message": ""
-                }
-                self._processes[self._current_process].append(message_dict)
+                if self._current_channel in self._channels:
+                    message_dict = {
+                        "statement": self._actual_statement,
+                        "channel": self._current_channel,
+                        "message": ""
+                    }
+                    self.add_message(message_dict)
+                else:
+                    self._current_channel = None
             elif self._current_channel:
-                self._processes[self._current_process][-1]["message"] = children_to_string(
-                    tree)
+                self.add_message(children_to_string(tree))
                 self._current_channel = None
+                if self._single_line_in_branch:
+                    self._single_line_in_branch = False
+                    self._current_depth -= 1
 
     def dot(self, _):
         self._current_process = None
@@ -44,25 +77,41 @@ class MessagesFinder(Visitor):
 
     def if_statement(self, _):
         self._current_channel = None
-        self._processes[self._current_process].append("if")
+        self.add_message({"if": []})
+        self._current_depth += 1
 
     def else_statement(self, _):
         self._current_channel = None
-        self._processes[self._current_process].append("else")
+        self.add_message({"else": []})
+        self._current_depth += 1
 
     def semicolon(self, _):
         self._current_channel = None
 
     def constant(self, tree):
         if self._current_process:
-            self._processes[self._current_process].append(
-                tree.children[0].value)
+            self.add_message(tree.children[0].value)
 
     def output(self):
         return deepcopy(self._processes)
 
     def visit(self, tree):
         raise NotImplementedError("Use visit_topdown instead")
+
+    def add_message(self, message):
+        current_list = self._processes[self._current_process]
+        if self._current_depth:
+            for _ in range(self._current_depth):
+                if "if" in current_list[-1]:
+                    current_list = current_list[-1]["if"]
+                else:
+                    current_list = current_list[-1]["else"]
+        if isinstance(message, str) and message.isnumeric():
+            current_list.append(message)
+        elif isinstance(message, str):
+            current_list[-1]["message"] += message
+        elif isinstance(message, dict):
+            current_list.append(message)
 
 
 class QueryFinder(Visitor):
@@ -73,13 +122,16 @@ class QueryFinder(Visitor):
         if self._current_process is None:
             self._current_process = tree.children[0].children[0].value
             self._processes[self._current_process] = []
-    
+
     def statement(self, tree):
-        if self._current_process:
+        print(tree, self._current_process)
+        if self._current_process and isinstance(tree.children[0], Tree):
             if tree.children[0].data == "func_content":
-                self._processes[self._current_process].append(children_to_string(tree))
+                self._processes[self._current_process].append(
+                    children_to_string(tree))
             else:
-                self._processes[self._current_process].append(tree.children[0].data)
+                self._processes[self._current_process].append(
+                    tree.children[0].data)
 
     def query(self, tree):
         if self._current_process is None:
@@ -92,8 +144,9 @@ class QueryFinder(Visitor):
     def output(self):
         processes = self._processes["query"][0].split(",")
         processes = [p.strip("(").strip(")") for p in processes]
-        output_dict = {k:v for k,v in self._processes.items() if k in processes}
-        for k,v in output_dict.items():
+        output_dict = {k: v for k, v in self._processes.items()
+                       if k in processes}
+        for k, v in output_dict.items():
             output_dict[k] = [x for x in v if x not in ["dot", "pipe"]]
         output_dict["query"] = processes
         return output_dict
